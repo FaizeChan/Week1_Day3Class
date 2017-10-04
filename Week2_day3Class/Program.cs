82620c10b3c6dee2d766ed5945e04d9c41085d0c
FaizeChan <obey.faize@gmail.com>
2017-10-04 10:52:52 -0400

    loops while and do while

--- Week2_day3Class/Program.cs
+++ Week2_day3Class/Program.cs
@@ -13,12 +13,55 @@ namespace Week2_day3Class
 
 
 
+            //Under is loops starting from 10 depleting by 1 all the way to 0
+
+            //for(int i = 10; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Section below is a while loop 
+
+            //Console.WriteLine("Do you want to play the game? (YES/NO)");
+            //string playAgain = Console.ReadLine();
+            //while(playAgain == "YES" )
+            //{
+            //    Console.WriteLine("It's a rematch!");
+            //    Console.WriteLine("do you want to play again? (YES/NO)");
+            //    playAgain = Console.ReadLine();
+
+            //}
 
 
+            //Console.WriteLine("Would you like to check in for your appointment?");
+            //string userAnswer = Console.ReadLine().ToLower();
+            //while (userAnswer == "yes")
+            //{
+            //    {
+            //        Console.WriteLine("What's your 6-digit patient ID");
+            //        {
+            //            Console.WriteLine("What's your appointment time?");
+            //            Console.WriteLine("INVALID TIME");
+            //        }
+            //        Console.WriteLine("INVALID ID NUMBER");
+            //        int unserID = int.Parse(Console.ReadLine());
+            //    }
+            //    Console.WriteLine("PATIENT CHECK IN SYSTEM");
+            //    userAnswer = Console.ReadLine();
+            //}
 
 
+            string playAgain;
 
+            do
+            {
+                Console.WriteLine("Great Game!");
+                Console.WriteLine("Do you want to play again? (YES/NO)");
+                playAgain = Console.ReadLine().ToUpper();
+            }
+            while (playAgain == "YES");
 
+            Console.WriteLine("Thanks for playing the game");
 
 
         }
