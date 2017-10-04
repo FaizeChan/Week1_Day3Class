069e8f86fc97eb2ab6930ced82eccdc75d82fdd5
FaizeChan <obey.faize@gmail.com>
2017-10-04 09:46:52 -0400

    first commit

---
+++ Week2_day3Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_day3Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
