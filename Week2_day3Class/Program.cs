c58ea558723fc775bab02e5107b5e4bc0adf6d50
FaizeChan <obey.faize@gmail.com>
2017-10-05 11:04:33 -0400

    initial commit Methods

--- Week2_day3Class/Program.cs
+++ Week2_day3Class/Program.cs
@@ -8,19 +8,54 @@ namespace Week2_day3Class
 {
     class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    int answer = Add(5, 7);
+
+        //    Console.WriteLine(answer);
+        //}
+
         static void Main(string[] args)
         {
-            int answer = Add(5, 7);
+            //Console.WriteLine("Please enter a name");
+            //string name = Console.ReadLine();
 
-            Console.WriteLine(answer);
+            //Console.WriteLine("Please enter the best food ever!");
+            //string food = Console.ReadLine();
+
+            //favoriteFood(name, food);
+            //favoriteFood("Faizon", "Lobster");
+            //favoriteFood("JD", "Pizza");
+            //favoriteFood("Monica", "shrimp");
+
+            RetirementCalculator(64);
         }
 
-        public static int Add(int firstNumber, int secondNumber)
+
+        public static void RetirementCalculator(int age)
         {
-            int sum = firstNumber + secondNumber;
+            int retirementAge = Math.Max(0, 65 - age);
 
-            return sum;
+            Console.WriteLine("The user will retire in " + retirementAge + " years");
         }
+
+        public static void favoriteFood(string name, string food)
+        {
+            Console.WriteLine("********************************");
+            Console.WriteLine(name + "'s favorite food is " + food);
+            Console.WriteLine();
+
+        }
+
+        // DO NOT REPEAT YOURSELF.
+
+
+        //public static int Add(int firstNumber, int secondNumber)
+        //{
+        //    int sum = firstNumber + secondNumber;
+
+        //    return sum;
+        //}
 
     }
 }
